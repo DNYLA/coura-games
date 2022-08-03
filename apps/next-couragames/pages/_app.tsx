00b8366af1cf07b52feebc4512a96c9c5e2e9fa7
Dan <22224922+DNYLA@users.noreply.github.com>
2022-08-03 01:23:43 +0100

    Added Chakra UI

--- apps/next-couragames/pages/_app.tsx
+++ apps/next-couragames/pages/_app.tsx
@@ -1,3 +1,10 @@
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  ChakraProvider,
+} from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
@@ -9,7 +16,9 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <title>Welcome to next-couragames!</title>
       </Head>
       <main className="app">
-        <Component {...pageProps} />
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
       </main>
     </>
   );
