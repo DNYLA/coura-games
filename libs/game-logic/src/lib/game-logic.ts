b54abb334df8a512d8d2463bd44fc40a439f9845
Dan <22224922+DNYLA@users.noreply.github.com>
2022-08-11 13:13:47 +0100

    added socket join backend

--- libs/game-logic/src/lib/game-logic.ts
+++ libs/game-logic/src/lib/game-logic.ts
@@ -1,18 +1,55 @@
-import { Games } from '@couragames/shared-types';
+import { Games, Player } from '@couragames/shared-types';
+import { Socket } from 'socket.io';
 import { Lobby } from '../types';
 
-export function createLobby(type: Games, hostId: string): Lobby {
-  const code = generateCode();
+const currentGames = new Map<string, Lobby>();
 
-  return {
+export function createLobby(socket: Socket, type: Games) {
+  const code = generateCode();
+  const randomNumber = generateCode();
+  const players: Player[] = [
+    {
+      id: socket.id,
+      username: `Guest-${randomNumber}`,
+      points: 0,
+      lastActivity: new Date(),
+    },
+  ];
+
+  const game: Lobby = {
     id: code,
-    hostId,
+    hostId: socket.id,
     type,
-    players: [],
+    players,
     maxPlayers: 2,
     started: false,
     lastActivity: new Date(),
   };
+
+  currentGames.set(code, game);
+  socket.data.gameId = code;
+
+  socket.emit('lobby_info', {
+    code,
+    maxPlayers: 2,
+    players: [],
+    started: false,
+    lastActivity: game.lastActivity,
+  });
+  socket.join(code);
+}
+
+export function joinLobby(socket: Socket, type: Games, id: string) {
+  console.log(type);
+  console.log(id);
+  const lobby = currentGames.get(id);
+
+  if (!lobby) {
+    socket.emit('join_lobby', { valid: false });
+    return;
+  }
+
+  socket.emit('join_lobby', {valid: true,  })
 }
 
 function generateCode() {
