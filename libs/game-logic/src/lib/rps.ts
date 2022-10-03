e0189d97ee88b4da0f883681467d2f7e45fe89d8
Dan <22224922+DNYLA@users.noreply.github.com>
2022-10-03 13:43:14 +0100

    Added Round Ended Timeoput

--- libs/game-logic/src/lib/rps.ts
+++ libs/game-logic/src/lib/rps.ts
@@ -10,7 +10,10 @@ export function main(lobby: Lobby, host: Socket) {
   d.setSeconds(d.getSeconds() + MAX_ROUND_TIME);
 
   //Call Timeout
-  setTimeout(() => roundEndedCallback(host, lobby.id), MAX_ROUND_TIME * 1000);
+  setTimeout(
+    () => roundEndedCallback(host, lobby.id, 0),
+    MAX_ROUND_TIME * 1000
+  );
 
   //Send Current Info
   lobby.data = { round: 0, timer: d };
@@ -21,18 +24,26 @@ export function main(lobby: Lobby, host: Socket) {
     currentRound: 0,
     timer: d.getTime(),
   };
-  host.to(lobby.id).emit('round_started', roundInfo);
-  host.emit('round_started', roundInfo);
+
+  host.to(lobby.id).emit('rps_round_started', roundInfo);
+  host.emit('rps_round_started', roundInfo);
   lobby.started = true; //Game is now live
 }
 
-function roundEndedCallback(socket: Socket, id: string) {
+function nextRoundCallback(socket: Socket, id: string, round: number) {}
+
+function roundEndedCallback(socket: Socket, id: string, round: number) {
   const lobby = validLobby(id, socket.id);
   if (!lobby) return;
   const ply = lobby.players.find((ply) => ply.id === socket.id);
   const moves = lobby.data;
+
   if (!moves.playerOneChoice && !moves.playerTwoChoice) {
-    return; //send no response/move ack
+    socket.emit('rps_round_ended', {
+      p1Move: null,
+      p2Move: null,
+      winner: null,
+    }); //send timeout message
   } else if (moves.playerOneChoice && !moves.playerTwoChoice) {
     return; //send Player One Wins
   } else if (!moves.playerOneChoice && moves.playerTwoChoice) {
@@ -61,8 +72,13 @@ export function calculateMove(socket: Socket, id: string, move: RPSMove) {
 
   ///Not a valid move. No need to acknowledge since uer has sent over invalid move meaning they are  attempting to cheat or exploit the system
   if (!(move in RPSMove)) return;
-  if (isPlayerOne) lobby.data.playerOneChoice = move;
-  else lobby.data.playerTwoChoice = move;
+
+  //Checking if the data is empty prevents cheating/users resending a "move" after selecting one.
+  if (isPlayerOne && !lobby.data.playerOneChoice)
+    lobby.data.playerOneChoice = move;
+  else if (!lobby.data.playerTwoChoice) lobby.data.playerTwoChoice = move;
+
+  //Verify if both players have made a choice
 }
 
 //Possibility to pass ID back then fetch
