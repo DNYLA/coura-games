48032aed4db95f6aeeb870a7bdb751e000133442
Dan <22224922+DNYLA@users.noreply.github.com>
2022-10-27 15:53:06 +0100

    Updated DM box

--- apps/next-couragames/components/Chat/DirectMessage.tsx
+++ apps/next-couragames/components/Chat/DirectMessage.tsx
@@ -44,39 +44,80 @@ export default function DirectMessage({ id, lastMessage }: DirectMessageProps) {
   ];
 
   return (
-    <Box>
-      {messages.map((msg, i) => (
-        <IndividualMessage
-          key={i}
-          content={msg.content}
-          name={msg.sender ? user.name : user2.name}
-          avatar={msg.sender ? user.avatar : user2.avatar}
-          sender={msg.sender}
-        />
-      ))}
-    </Box>
+    <Container>
+      <MessageLog>
+        {messages.map((msg) => (
+          <IndividualMessage
+            content={msg.content}
+            name={msg.sender ? user.name : user2.name}
+            avatar={msg.sender ? user.avatar : user2.avatar}
+            sender={msg.sender}
+          />
+        ))}
+      </MessageLog>
+      <InputBox type="text" name="name" placeholder="Enter message..." />
+    </Container>
   );
 }
+
+const InputBox = styled.input`
+  width: 90%;
+  height: 30px;
+  margin: 5px;
+  border-radius: 5px;
+  font-size: 15px;
+  padding: 5px;
+  :focus {
+    outline: 0;
+    border: 1px solid #161819;
+  }
+
+  border: 1px solid #141617;
+
+  transition: all 300ms ease-in;
+`;
+
+const Container = styled.div`
+  /* display: flex; */
+  /* flex-direction: column; */
+  /* justify-content: space-around; */
+`;
+
+const MessageLog = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 interface MessageProps {
   name: string;
   content: string;
   avatar: string;
   sender: boolean;
 }
-function IndividualMessage({ content, name, avatar, sender }: MessageProps) {
+function IndividualMessage({ content, sender }: MessageProps) {
   return (
-    <Flex>
-      <MessageBox>
+    <MessageCon>
+      <MessageBox sender={sender}>
         <span>{content}</span>
       </MessageBox>
-    </Flex>
+    </MessageCon>
   );
 }
 
-const MessageBox = styled.div`
+interface MessagesProps {
+  sender: boolean;
+}
+
+const MessageCon = styled.div`
+  display: inline-block;
+  /* display: flex; */
+  /* flex-direction: column; */
+`;
+
+const MessageBox = styled.div<MessagesProps>`
   padding: 5px;
   margin: 5px;
   background-color: rgba(0, 0, 0, 0.2);
+  background-color: ${(props) => (props.sender ? '#00000033' : '#1f94e0')};
   border-radius: 10px;
   span {
     padding: 5px;
