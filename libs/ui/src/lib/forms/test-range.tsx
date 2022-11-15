ce204a56c1ad0591793cd833549f57cf8713bb6d
Dan <22224922+DNYLA@users.noreply.github.com>
2022-11-15 21:29:43 +0000

    Added More Form Components

--- libs/ui/src/lib/forms/test-range.tsx
+++ libs/ui/src/lib/forms/test-range.tsx
@@ -1,45 +1,72 @@
 import styled from '@emotion/styled';
-import React from 'react';
 
-interface RangeProps {
-  title: string;
+export interface IOnInputRangeChange {
+  (event: React.ChangeEvent<HTMLInputElement>): void;
+}
+/* eslint-disable-next-line */
+export interface InputRangeProps {
+  value: number;
+  infoMessage?: string | React.ReactNode;
+  message: string;
   min: number;
   max: number;
+  onChange: IOnInputRangeChange;
+  disabled?: boolean;
+  prefix?: string;
   step: number;
-  value: number;
-  onChange: (value: number) => void;
 }
 
-export default function Range({
-  title,
-  min,
-  max,
-  step,
+export function InputRange({
+  message,
   value,
   onChange,
-}: RangeProps) {
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    onChange(Number(e.target.value));
-  };
-
+  disabled,
+  min,
+  max,
+  prefix,
+  step,
+}: InputRangeProps) {
   return (
-    <Container>
-      <StyledRange
-        type="range"
-        min={min}
-        max={max}
-        step={step}
-        value={value}
-        onChange={handleChange}
-      />
-      <h1>{value}</h1>
-    </Container>
+    <>
+      <StyledInputBox>
+        <StyledInputRange
+          type="range"
+          min={min}
+          max={max}
+          step={step}
+          value={value}
+          onChange={(e) => onChange(e)}
+          disabled={disabled}
+        />
+        <h2>
+          {prefix}
+          {value}
+        </h2>
+      </StyledInputBox>
+    </>
   );
 }
 
+const StyledInputBox = styled.div`
+  user-select: none;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  input {
+    /* width: 93%; */
+  }
+  h1 {
+    padding-left: 10px;
+    /* width: 7%; */
+  }
+`;
+
+// excess height to improve interactive area / accessibility
 const height = '36px';
 const thumbHeight = 36;
 const trackHeight = '16px';
+
+// colours
 
 const makeLongShadow = (color: any, size: any) => {
   let i = 18;
@@ -48,21 +75,11 @@ const makeLongShadow = (color: any, size: any) => {
   for (; i < 706; i++) {
     shadow = `${shadow}, ${i}px 0 0 ${size} ${color}`;
   }
+
   return shadow;
 };
 
-const Container = styled.div`
-  user-select: none;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-
-  h1 {
-    padding-left: 10px;
-  }
-`;
-
-const StyledRange = styled.input`
+const StyledInputRange = styled.input`
   /* width: 100%; */
   overflow: hidden;
   display: block;
@@ -75,7 +92,6 @@ const StyledRange = styled.input`
   background: #202225;
   border-radius: 15px;
   cursor: pointer;
-
   &:focus {
     outline: none;
   }
@@ -83,15 +99,14 @@ const StyledRange = styled.input`
   &::-webkit-slider-runnable-track {
     width: 100%;
     height: ${height};
-    background: ${`linear-gradient(to bottom, #ff9929, #ff9929) 100% 50% / 100% 16px no-repeat transparent`};
+    background: 'linear-gradient(to bottom, #ff9929, #ff9929) 100% 50% / 100% 16px no-repeat transparent';
   }
-
   &::-webkit-slider-thumb {
     position: relative;
     appearance: none;
     height: ${thumbHeight}px;
     width: ${thumbHeight}px;
-    background: #8d8d8e;
+    background: '#8d8d8e';
     border-radius: 100%;
     border: 0;
     top: 50%;
@@ -99,29 +114,25 @@ const StyledRange = styled.input`
     box-shadow: ${(p) => makeLongShadow('#141617', '-10px')};
     transition: background-color 150ms;
   }
-
   &::-moz-range-track,
   &::-moz-range-progress {
     width: 100%;
     height: ${height};
-    background: ${`linear-gradient(to bottom, #141617, #141617) 100% 50% / 100% 16px no-repeat transparent`};
+    background: 'linear-gradient(to bottom, #141617, #141617) 100% 50% / 100% 16px no-repeat transparent';
   }
-
   &::-moz-range-progress {
-    background: ${`linear-gradient(to bottom, #ff9929, #ff9929) 100% 50% / 100% 16px no-repeat transparent`};
+    background: 'linear-gradient(to bottom, #ff9929, #ff9929) 100% 50% / 100% 16px no-repeat transparent';
   }
-
   &::-moz-range-thumb {
     appearance: none;
     margin: 0;
     height: ${thumbHeight};
     width: ${thumbHeight};
-    background: #8d8d8e;
+    background: '#8d8d8e';
     border-radius: 100%;
     border: 0;
     transition: background-color 150ms;
   }
-
   &::-ms-track {
     width: 100%;
     height: ${height};
@@ -130,14 +141,12 @@ const StyledRange = styled.input`
     color: transparent;
     background: transparent;
   }
-
   &::-ms-fill-lower {
-    background: ${`linear-gradient(to bottom, #ff9929, #ff9929) 100% 50% / 100% 16px no-repeat transparent`};
+    background: 'linear-gradient(to bottom, #ff9929, #ff9929) 100% 50% / 100% 16px no-repeat transparent';
   }
   &::-ms-fill-upper {
-    background: ${`linear-gradient(to bottom, #141617, #141617) 100% 50% / 100% 16px no-repeat transparent`};
+    background: 'linear-gradient(to bottom, #141617, #141617) 100% 50% / 100% 16px no-repeat transparent';
   }
-
   &::-ms-thumb {
     appearance: none;
     height: ${thumbHeight};
@@ -151,7 +160,6 @@ const StyledRange = styled.input`
     margin: 0;
     box-shadow: none;
   }
-
   &:hover,
   &:focus {
     &::-webkit-slider-thumb {
