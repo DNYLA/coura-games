2ab7078737bd8a19a530a869c3a4b082d73c6ec1
Dan <22224922+DNYLA@users.noreply.github.com>
2022-11-17 15:11:14 +0000

    Added Redis

---
+++ apps/api/src/redis.ts
@@ -0,0 +1,16 @@
+import * as Redis from 'redis';
+
+const redis = Redis.createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  password: process.env.REDIS_PASSWORD,
+});
+
+redis.on('connect', async () => {
+  console.log('Redis: Connected');
+});
+
+redis.on('error', (error) => {
+  console.log(error);
+});
+
+export { redis };
