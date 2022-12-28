ec798f7932678e7cb030ad5f668c9b6eaba9e04b
Dan <22224922+DNYLA@users.noreply.github.com>
2022-12-28 23:20:50 +0000

    Added Comment Section

--- apps/next-couragames/pages/member/SelfProfile.tsx
+++ apps/next-couragames/pages/member/SelfProfile.tsx
@@ -1,5 +1,11 @@
 import styled from '@emotion/styled';
-import React from 'react';
+import React, { useContext } from 'react';
+import { BsPlusLg } from 'react-icons/bs';
+import { BiMinus } from 'react-icons/bi';
+import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
+import { FiEdit, FiMinus, FiPlus } from 'react-icons/fi';
+import { MdEdit } from 'react-icons/md';
+import UserContext from 'apps/next-couragames/context/auth';
 
 export default function SelfProfile() {
   return (
@@ -7,6 +13,8 @@ export default function SelfProfile() {
       <Container>
         <Main>
           <ProfHeader />
+          <div>Time Stat Info Here</div>
+          <Comments />
         </Main>
         <Sidebar>Sidebar</Sidebar>
       </Container>
@@ -15,10 +23,13 @@ export default function SelfProfile() {
 }
 
 function ProfHeader() {
+  const url =
+    'https://images.complex.com/complex/images/c_fill,dpr_2.0,f_auto,q_auto,w_1400/fl_lossy,pg_1/ved7hojt36yiabmf7bwc/roddy-dec-14?fimg-ssr';
+
   return (
     <ProfileHeader>
       <Avatar>
-        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
+        <img src={url} />
       </Avatar>
       <ProfileContent>
         <h2>Darnius_Latwon</h2>
@@ -45,6 +56,214 @@ function ProfHeader() {
     </ProfileHeader>
   );
 }
+
+interface CommentProps {
+  message: string;
+  likes: number;
+  dislikes: number;
+  date: Date;
+  author: string;
+  author_avatar: string;
+}
+
+function Comment({
+  message,
+  likes,
+  dislikes,
+  date,
+  author,
+  author_avatar,
+}: CommentProps) {
+  const img_ur =
+    'https://images.complex.com/complex/images/c_fill,dpr_2.0,f_auto,q_auto,w_1400/fl_lossy,pg_1/ved7hojt36yiabmf7bwc/roddy-dec-14?fimg-ssr';
+  const { user } = useContext(UserContext);
+
+  const isOwner = author.toLowerCase() === user.username.toLowerCase();
+
+  return (
+    <CommentContainer>
+      <CommentUpvotes>
+        <FiPlus />
+        <p>{likes - dislikes}</p>
+        <FiMinus />
+      </CommentUpvotes>
+      <Message>
+        <CommentPanel>
+          <CommentAuthor>
+            <img src={img_ur}></img>
+            <p>{author}</p>
+            <span>Three Weeks Ago</span>
+          </CommentAuthor>
+          {isOwner && (
+            <div className="comment_edit">
+              <CommentButton>
+                <MdEdit />
+                <p>Delete</p>
+              </CommentButton>
+
+              <CommentButton primary>
+                <MdEdit />
+                <p>Edit</p>
+              </CommentButton>
+            </div>
+          )}
+        </CommentPanel>
+
+        <p>{message}</p>
+      </Message>
+    </CommentContainer>
+  );
+}
+
+const CommentButton = styled.div<{ primary?: boolean }>`
+  display: flex;
+  /* justify-content: center; */
+  align-items: center;
+  gap: 6px;
+  cursor: pointer;
+
+  p,
+  svg {
+    color: ${(props) =>
+      props.primary ? 'hsl(238, 40%, 52%)' : 'hsl(358, 79%, 66%)'};
+    /* color: hsl(238, 40%, 52%); */
+  }
+
+  :hover {
+    font-size: 18px;
+    transition: ease-in all 100ms;
+
+    svg {
+      /* color: ${(props) =>
+        props.primary ? 'hsl(211, 10%, 45%)' : 'hsl(357, 100%, 86%)'}; */
+    }
+  }
+
+  transition: ease-out all 150ms;
+`;
+
+const CommentPanel = styled.div`
+  display: flex;
+  overflow: auto;
+  width: 100%;
+
+  .comment_edit {
+    display: flex;
+    align-items: center;
+    margin-bottom: 15px;
+    margin-left: auto;
+    justify-content: flex-end;
+    gap: 15px;
+
+    p {
+      text-align: center;
+    }
+  }
+`;
+
+const CommentUpvotes = styled.div`
+  padding: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  border-radius: 5px;
+  gap: 10px;
+  background-color: hsl(223, 19%, 93%);
+
+  max-height: 130px;
+
+  svg {
+    font-size: 24px;
+    /* opacity: 60%; */
+    color: hsl(239, 57%, 85%);
+    cursor: pointer;
+  }
+`;
+
+const CommentContainer = styled.div`
+  display: flex;
+  border-radius: 5px;
+  background-color: white;
+  color: black;
+  padding: 25px;
+  gap: 30px;
+`;
+
+const Message = styled.div`
+  width: 100%;
+  p {
+    color: hsl(211, 10%, 45%);
+    font-weight: 500;
+  }
+`;
+
+const CommentAuthor = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
+  float: left;
+  img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin-right: 15px;
+    object-fit: cover;
+  }
+
+  p {
+    font-weight: 500;
+    font-size: 18px;
+    margin-right: 25px;
+    color: hsl(212, 24%, 26%);
+  }
+
+  span {
+    font-weight: 500;
+    /* opacity: 60%; */
+    font-size: 14px;
+    color: hsl(211, 10%, 45%);
+  }
+`;
+
+function Comments() {
+  const message =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magnam vitae itaque quae porro voluptatibus minus hic! Soluta suscipit laudantium fugiat vero tempora, ipsum accusantium illum ea vel quod molestias ut repellat, natus ducimus eaque obcaecati consequuntur libero doloremque cupiditate est dicta nam? Dicta animi libero amet officia totam quisquam!';
+  return (
+    <CommentBox>
+      {/* Comments */}
+      <Comment
+        message={'Welcome to this community!'}
+        likes={35}
+        dislikes={2}
+        date={new Date()}
+        author={'Dan'}
+        author_avatar={'damze'}
+      />
+
+      <Comment
+        message={message}
+        likes={2}
+        dislikes={3}
+        date={new Date()}
+        author={'John2'}
+        author_avatar={'damze'}
+      />
+    </CommentBox>
+  );
+}
+
+const CommentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: 'Rubik', sans-serif;
+  margin-top: 50px;
+  /* background-color: red; */
+  /* border-radius: inherit; */
+  border-radius: 5px;
+  gap: 10px;
+`;
 
 const Main = styled.div`
   /* padding: 1rem; */
@@ -110,7 +329,7 @@ const Avatar = styled.div`
     height: 90px;
     border-radius: 50%;
     border: 5px solid white;
-
+    object-fit: cover;
     margin-bottom: -70px;
   }
 `;
