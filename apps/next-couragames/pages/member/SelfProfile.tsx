10962a93bd98fc476f6222af99bdfaf12407de6c
Dan <22224922+DNYLA@users.noreply.github.com>
2022-12-29 00:42:53 +0000

    Updated Comment Section

--- apps/next-couragames/pages/member/SelfProfile.tsx
+++ apps/next-couragames/pages/member/SelfProfile.tsx
@@ -1,11 +1,12 @@
 import styled from '@emotion/styled';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { BsPlusLg } from 'react-icons/bs';
 import { BiMinus } from 'react-icons/bi';
 import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
 import { FiEdit, FiMinus, FiPlus } from 'react-icons/fi';
 import { MdEdit } from 'react-icons/md';
 import UserContext from 'apps/next-couragames/context/auth';
+import moment from 'moment';
 
 export default function SelfProfile() {
   return (
@@ -24,7 +25,7 @@ export default function SelfProfile() {
 
 function ProfHeader() {
   const url =
-    'https://images.complex.com/complex/images/c_fill,dpr_2.0,f_auto,q_auto,w_1400/fl_lossy,pg_1/ved7hojt36yiabmf7bwc/roddy-dec-14?fimg-ssr';
+    'https://yt3.ggpht.com/oHmF1pFUKwRFGsUbzNYKkbhIJoaW4b-L_IZ0HZnmerBL_MdLwiVSaCg2YgNzj8LI8HXF59SYk8U=s900-c-k-c0x00ffffff-no-rj';
 
   return (
     <ProfileHeader>
@@ -33,7 +34,7 @@ function ProfHeader() {
       </Avatar>
       <ProfileContent>
         <h2>Darnius_Latwon</h2>
-        <p>Winning games in my sleep zzz....</p>
+        <p>Winning games in my sleep....</p>
       </ProfileContent>
       <MemberStats>
         <StatItem>
@@ -58,28 +59,16 @@ function ProfHeader() {
 }
 
 interface CommentProps {
-  message: string;
-  likes: number;
-  dislikes: number;
-  date: Date;
-  author: string;
-  author_avatar: string;
+  comment: Comment;
+  handleDelete: (id: number) => void;
 }
 
-function Comment({
-  message,
-  likes,
-  dislikes,
-  date,
-  author,
-  author_avatar,
-}: CommentProps) {
-  const img_ur =
-    'https://images.complex.com/complex/images/c_fill,dpr_2.0,f_auto,q_auto,w_1400/fl_lossy,pg_1/ved7hojt36yiabmf7bwc/roddy-dec-14?fimg-ssr';
+function UserComment({ comment, handleDelete }: CommentProps) {
+  const { id, message, likes, dislikes, date, author, author_avatar } = comment;
   const { user } = useContext(UserContext);
-
   const isOwner = author.toLowerCase() === user.username.toLowerCase();
 
+  const now = moment(date).fromNow();
   return (
     <CommentContainer>
       <CommentUpvotes>
@@ -90,13 +79,20 @@ function Comment({
       <Message>
         <CommentPanel>
           <CommentAuthor>
-            <img src={img_ur}></img>
+            <img
+              src={author_avatar}
+              onError={(e) => {
+                e.stopPropagation();
+                e.currentTarget.src =
+                  'https://www.pngitem.com/pimgs/m/421-4212341_default-avatar-svg-hd-png-download.png';
+              }}
+            ></img>
             <p>{author}</p>
-            <span>Three Weeks Ago</span>
+            <span>{now}</span>
           </CommentAuthor>
           {isOwner && (
             <div className="comment_edit">
-              <CommentButton>
+              <CommentButton onClick={() => handleDelete(id)}>
                 <MdEdit />
                 <p>Delete</p>
               </CommentButton>
@@ -227,32 +223,152 @@ const CommentAuthor = styled.div`
   }
 `;
 
+type Comment = {
+  id: number;
+  message: string;
+  likes: number;
+  dislikes: number;
+  date: Date;
+  author: string;
+  author_avatar: string;
+};
+
 function Comments() {
   const message =
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magnam vitae itaque quae porro voluptatibus minus hic! Soluta suscipit laudantium fugiat vero tempora, ipsum accusantium illum ea vel quod molestias ut repellat, natus ducimus eaque obcaecati consequuntur libero doloremque cupiditate est dicta nam? Dicta animi libero amet officia totam quisquam!';
+  const img_ur =
+    'https://yt3.ggpht.com/oHmF1pFUKwRFGsUbzNYKkbhIJoaW4b-L_IZ0HZnmerBL_MdLwiVSaCg2YgNzj8LI8HXF59SYk8U=s900-c-k-c0x00ffffff-no-rj';
+
+  const { user } = useContext(UserContext);
+
+  const [fetchedComments, setComments] = useState<Comment[]>([
+    {
+      id: 0,
+      message: 'Welcome to this community!',
+      likes: 35,
+      dislikes: 2,
+      date: new Date('12/29/2022'),
+      author: 'Dan',
+      author_avatar: img_ur,
+    },
+    {
+      id: 1,
+      message,
+      likes: 2,
+      dislikes: 3,
+      date: new Date('11/29/2022'),
+      author: 'John2',
+      author_avatar:
+        'https://api.time.com/wp-content/uploads/2020/02/Roddy-Ricch-the-Box-interview.jpg?quality=85&w=2000',
+    },
+  ]);
+
+  const postComment = () => {
+    console.log('Running');
+
+    const curComments = [...fetchedComments];
+    if (curComments[0].message === newMessage || !newMessage) return; //Throw Error -> Show a message to user
+    curComments.unshift({
+      id: fetchedComments.length - 1,
+      message: newMessage,
+      likes: 0,
+      dislikes: 0,
+      date: new Date(),
+      author: user.username,
+      author_avatar: user.avatarUrl,
+    });
+    setComments(curComments);
+  };
+
+  const deleteComment = (id: number) => {
+    const curComments = [...fetchedComments];
+    const index = curComments.findIndex((c) => c.id === id);
+
+    if (index === -1) return; //Unfindable || Already Deleted
+
+    curComments.splice(index, 1);
+    setComments(curComments);
+  };
+
+  const [newMessage, setNewMessage] = useState('');
+
   return (
     <CommentBox>
       {/* Comments */}
-      <Comment
-        message={'Welcome to this community!'}
-        likes={35}
-        dislikes={2}
-        date={new Date()}
-        author={'Dan'}
-        author_avatar={'damze'}
-      />
-
-      <Comment
-        message={message}
-        likes={2}
-        dislikes={3}
-        date={new Date()}
-        author={'John2'}
-        author_avatar={'damze'}
-      />
+      <PostComment>
+        <img src={img_ur}></img>
+        <textarea
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          placeholder="Enter your comment..."
+        />
+        <button onClick={postComment}>POST</button>
+      </PostComment>
+      {fetchedComments.map((comment) => (
+        <UserComment
+          key={comment.id}
+          comment={comment}
+          handleDelete={deleteComment}
+        />
+      ))}
     </CommentBox>
   );
 }
+
+const PostComment = styled.div`
+  display: flex;
+  border-radius: 5px;
+  background-color: white;
+  color: black;
+  padding: 25px;
+  gap: 30px;
+  align-items: center;
+
+  img {
+    width: 75px;
+    height: 75px;
+    border-radius: 50%;
+    object-fit: cover;
+  }
+
+  textarea {
+    width: 100%;
+    resize: none;
+    padding: 15px;
+    background: transparent;
+    border: 1px solid black;
+    border-radius: 4px;
+    color: hsl(211, 10%, 45%);
+    /* font-weight: 400; */
+    :focus {
+      outline: none;
+      /* transition: ease-in 200ms all; */
+      border: 1px solid hsl(239, 57%, 85%);
+    }
+
+    ::placeholder {
+      color: hsl(211, 10%, 45%);
+    }
+
+    transition: ease-out 100ms all;
+  }
+
+  /* Rework CSS below isn't upto standards */
+  button {
+    display: flex;
+    height: 20px;
+    align-items: center;
+    background: hsl(238, 40%, 52%);
+    color: white;
+    border-radius: 5px;
+    padding: 25px;
+
+    :hover {
+      /* background-color: #6d87a2; */
+      background-color: hsl(211, 10%, 45%);
+    }
+  }
+`;
 
 const CommentBox = styled.div`
   display: flex;
