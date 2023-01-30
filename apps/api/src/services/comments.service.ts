ac4a9573b1f01e4ef939cc2d82d1f99b6a17c59e
Dan <22224922+DNYLA@users.noreply.github.com>
2023-01-30 18:33:15 +0000

    Users can create + delete comments

--- apps/api/src/services/comments.service.ts
+++ apps/api/src/services/comments.service.ts
@@ -1,5 +1,5 @@
 import { PublicUser } from '@couragames/shared-types';
-import { PrismaClient, Comment, User } from '@prisma/client';
+import { PrismaClient, Comment, User, Prisma } from '@prisma/client';
 import { UserService } from './user.service';
 
 export class CommentsService implements ICommentsService {
@@ -12,11 +12,27 @@ export class CommentsService implements ICommentsService {
   async getComments(username: string, page?: number): Promise<Comment[]> {
     const userId = await this.userService.userIdFromName(username);
     const comments = await this.prisma.comment.findMany({
-      where: { toUserId: userId, deleted: false },
+      where: { toUserId: userId },
       orderBy: { createdAt: 'desc' },
     });
 
     return comments;
+  }
+
+  async createComment(
+    username: string,
+    authorId: number,
+    message: string
+  ): Promise<Comment> {
+    const userId = await this.userService.userIdFromName(username);
+
+    //TODO: Add Check if User is Blocked?
+
+    const comment = await this.prisma.comment.create({
+      data: { authorId, toUserId: userId, content: message },
+    });
+
+    return comment;
   }
 
   async editComment(
@@ -25,7 +41,7 @@ export class CommentsService implements ICommentsService {
     userId: number
   ): Promise<boolean> {
     const comment = await this.getComment(id);
-    if (comment.fromUserId !== userId) return false;
+    if (comment.authorId !== userId) return false;
 
     await this.prisma.comment.update({
       where: { id },
@@ -40,21 +56,35 @@ export class CommentsService implements ICommentsService {
 
   async deleteComment(id: number, userId: number): Promise<boolean> {
     const comment = await this.getComment(id);
-    if (comment.fromUserId !== userId) return false;
+    if (comment.authorId !== userId) return false;
 
-    await this.prisma.comment.update({
+    await this.prisma.comment.delete({
       where: { id },
-      data: {
-        deleted: true,
-        deletedAt: new Date(),
-      },
     });
 
     return true;
   }
 
-  interactComment(id: string, upvote: boolean): Promise<number> {
-    throw new Error('Method not implemented.');
+  async interactComment(
+    id: number,
+    upvote: boolean,
+    userId: number
+  ): Promise<number> {
+    const comment = await this.getComment(id);
+    const curVal = comment.likes - comment.dislikes;
+    if (comment.authorId !== userId) return curVal;
+
+    const newValue = upvote ? curVal + 1 : curVal - 1; //Checks if user upvoted and increments
+    const data: Prisma.CommentUpdateInput = upvote
+      ? { likes: { increment: 1 } }
+      : { dislikes: { increment: 1 } };
+
+    await this.prisma.comment.update({
+      where: { id },
+      data,
+    });
+
+    return newValue;
   }
 
   private async getComment(id: number): Promise<Comment> {
@@ -63,8 +93,13 @@ export class CommentsService implements ICommentsService {
 }
 
 interface ICommentsService {
-  getComments(userId: string, page?: number): Promise<Comment[]>;
+  getComments(username: string, page?: number): Promise<Comment[]>;
+  createComment(
+    username: string,
+    authorId: number,
+    message: string
+  ): Promise<Comment>;
   editComment(id: number, content: string, userId: number): Promise<boolean>;
   deleteComment(id: number, userId: number): Promise<boolean>;
-  interactComment(id: string, upvote: boolean): Promise<number>;
+  interactComment(id: number, upvote: boolean, userId: number): Promise<number>;
 }
--- libs/ui/src/api.ts
+++ libs/ui/src/api.ts
@@ -1,7 +1,13 @@
 import { Comment, Comments, PublicUser, User } from '@couragames/shared-types';
 import axios, { AxiosRequestConfig } from 'axios';
 import { GetServerSidePropsContext } from 'next';
-import { getAPIUrl } from '../index'; //Looks weird as just '..' so i added the index part
+
+const getAPIUrl = () => {
+  const url = process.env['NX_API_URL'];
+  if (!url) throw 'API URL not defined';
+
+  return url;
+};
 
 const CONFIG: AxiosRequestConfig = {
   withCredentials: true,
@@ -21,4 +27,10 @@ export const fetchUserProfile = (username: string) =>
   AXIOS.get<PublicUser>(`/member/${username}`);
 
 export const fetchComments = (username: string, page?: number) =>
-  AXIOS.get<Comments>(`/member/${username}/comments?page=${page ?? 0}`);
+  AXIOS.get<Comments>(`/member/${username}/comment?page=${page ?? 0}`);
+
+export const postComment = (username: string, message: string) =>
+  AXIOS.post<Comment>(`/member/${username}/comment`, { message });
+
+export const deleteComment = (username: string, id: number) =>
+  AXIOS.delete<void>(`/member/${username}/comment?id=${id}`);
