64499f38254a7319220a2ed311c0b80369e3ce93
Dan <22224922+DNYLA@users.noreply.github.com>
2023-02-02 13:22:40 +0000

    Profile has settings

--- libs/ui/src/lib/profile/header.tsx
+++ libs/ui/src/lib/profile/header.tsx
@@ -1,40 +1,123 @@
 import { PublicUser } from '@couragames/shared-types';
 import styled from '@emotion/styled';
+import UserContext from 'libs/ui/src/lib/context/auth';
+import SettingsModal from './SettingsModal';
 import moment from 'moment';
+import { useContext, useState } from 'react';
+import { AiFillEdit, AiOutlineUserAdd } from 'react-icons/ai';
+import { ImBlocked } from 'react-icons/im';
+import { useDisclosure } from '@chakra-ui/react';
 interface HeaderProps {
   member: PublicUser;
+  selfName: string;
 }
 
-export function ProfileHeader({ member }: HeaderProps) {
-  return (
-    <Container>
-      <Avatar>
-        <img src={member.avatarUrl} alt="Member's Avatar" />
-      </Avatar>
-      <Content>
-        <h2>{member.username}</h2>
-        <p>{member.status}</p>
-      </Content>
+enum ButtonType {
+  Success,
+  Negative,
+  INFO,
+}
 
-      <Info>
-        <Item>
-          <h3>Points</h3>
-          <p>{member.points}</p>
-        </Item>
-        {/* <StatItem>
+const typeToColor = (type: ButtonType) => {
+  switch (type) {
+    default:
+      return '#000';
+    case ButtonType.Success:
+      return 'hsl(123, 40%, 52%)';
+    case ButtonType.Negative:
+      return 'hsl(0, 42%, 52%)';
+    case ButtonType.INFO:
+      return 'hsl(205, 42%, 52%)';
+  }
+};
+
+const ActionSettings = styled.div`
+  display: flex;
+  justify-content: end;
+  gap: 5px;
+  padding: 15px 15px;
+`;
+
+const ActionButton = styled.button<{ action: ButtonType }>`
+  display: flex;
+  height: 20px;
+  align-items: center;
+  background: ${(props) => typeToColor(props.action)};
+  color: white;
+  border-radius: 5px;
+  padding: 25px;
+  cursor: pointer;
+
+  :hover {
+    /* background-color: #6d87a2; */
+    background-color: hsl(211, 10%, 45%);
+  }
+`;
+
+export function ProfileHeader({ member, selfName }: HeaderProps) {
+  const { user } = useContext(UserContext);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  console.log('In Member Profile');
+  console.log(user);
+
+  const isUser = selfName.toLowerCase() === member.username.toLowerCase();
+
+  return (
+    <>
+      <SettingsModal
+        open={isOpen}
+        onClose={onClose}
+        avatarURL={member.avatarUrl}
+      />
+      <Container>
+        <Avatar>
+          <img src={member.avatarUrl} alt="Member's Avatar" />
+        </Avatar>
+
+        <ActionSettings>
+          {!isUser && (
+            <>
+              <ActionButton action={ButtonType.Negative}>
+                <ImBlocked />
+              </ActionButton>
+              <ActionButton action={ButtonType.Success}>
+                <AiOutlineUserAdd />
+              </ActionButton>
+            </>
+          )}
+          {isUser && (
+            <ActionButton action={ButtonType.INFO} onClick={onOpen}>
+              <AiFillEdit style={{ cursor: 'pointer' }} />
+            </ActionButton>
+          )}
+        </ActionSettings>
+
+        <Content>
+          <h2>{member.username}</h2>
+          <p>{member.status}</p>
+        </Content>
+
+        <Info>
+          <Item>
+            <h3>Points</h3>
+            <p>{member.points}</p>
+          </Item>
+          {/* <StatItem>
           <h3>Leagues</h3>
           <p>25</p>
         </StatItem>{' '} */}
-        <Item>
-          <h3>Followers</h3>
-          <p>33</p>
-        </Item>
-        <Item>
-          <h3>Joined</h3>
-          <p>{moment(member.joined).format('MMM Do, YYYY')}</p>
-        </Item>
-      </Info>
-    </Container>
+          <Item>
+            <h3>Followers</h3>
+            <p>33</p>
+          </Item>
+          <Item>
+            <h3>Joined</h3>
+            <p>{moment(member.joined).format('MMM Do, YYYY')}</p>
+          </Item>
+        </Info>
+      </Container>
+    </>
   );
 }
 
@@ -47,6 +130,14 @@ const Container = styled.div`
   color: black;
   padding-bottom: 15px;
   border-radius: 8px;
+
+  .settings {
+    /* width: 25px; */
+    /* height: 25px; */
+    display: flex;
+    justify-content: end;
+    padding: 15px 15px;
+  }
 `;
 
 const Avatar = styled.div`
