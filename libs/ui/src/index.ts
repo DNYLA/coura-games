c4893e7852957d3b4f9a0feba68edad1e495e2e7
Dan <22224922+DNYLA@users.noreply.github.com>
2023-02-07 13:31:08 +0000

    Added FileUpload

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,6 +1,7 @@
 export * from './lib/ui';
 export * from './lib/forms/checkbox';
 export * from './lib/forms/range';
+export * from './lib/forms/FileUpload';
 
 export * from './lib/profile/header';
 export * from './lib/profile/stats';
