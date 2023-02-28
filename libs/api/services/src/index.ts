fd04c37f0ba5b09158642b9cda46a2809c37690c
Dan <22224922+DNYLA@users.noreply.github.com>
2023-02-28 22:35:35 +0000

    Generate USers

--- libs/api/services/src/index.ts
+++ libs/api/services/src/index.ts
@@ -2,4 +2,5 @@ export * from './lib/comments.service';
 export * from './lib/prisma.service';
 export * from './lib/user.service';
 export * from './lib/match.service';
+export * from './lib/leaderboard.service';
 export * from './lib/socket.server';
