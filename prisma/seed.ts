6fa34dac58a0b8875fa2d3da4ae635cc51745052
Dan <22224922+DNYLA@users.noreply.github.com>
2023-02-28 22:35:35 +0000

    Generate USers

---
+++ prisma/seed.ts
@@ -0,0 +1,32 @@
+import { GameType, PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+import { UserStats } from '../libs/ui/src';
+const prisma = new PrismaClient();
+async function main() {
+  for (let i = 0; i < 500; i++) {
+    const statObject = {};
+
+    Object.keys(GameType).map((type) => {
+      statObject[type] = Math.floor(Math.random() * 650);
+    });
+
+    prisma.user.create({
+      data: {
+        username: faker.internet.userName(),
+        password: faker.internet.password(),
+        avatarUrl: faker.internet.avatar(),
+        status: faker.company.catchPhrase(),
+        stats: statObject,
+      },
+    });
+  }
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
