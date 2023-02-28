d0b5b174953827b0de7398bfad9ed444c113eab5
Dan <22224922+DNYLA@users.noreply.github.com>
2023-02-28 22:35:35 +0000

    Generate USers

--- libs/api/services/src/lib/leaderboard.service.ts
+++ libs/api/services/src/lib/leaderboard.service.ts
@@ -1,9 +1,43 @@
+import { Leaderboard, LeaderboardStat } from '@couragames/shared-types';
+import { GameType } from '@prisma/client';
 import { prisma } from 'libs/api/services/src/lib/prisma.service';
 
+type FilterStats = LeaderBoar;
 export class LeaderboardService {
-  async fetchLeaderboards() {
+  static async fetchLeaderboards() {
+    //Check Cache
+
     const users = await prisma.user.findMany({
       select: { id: true, username: true, avatarUrl: true, stats: true },
+      where: { stats: { not: {} } },
     });
+
+    const leaderboardmap = new Map<
+      string,
+      { lowest: number; values: LeaderboardStat[] }
+    >();
+
+    Object.keys(GameType).map((type) => {
+      leaderboardmap.set(type.toString(), { lowest: 0, values: [] });
+    });
+
+    users.forEach((user) => {
+      const stats = user.stats;
+      Object.keys(stats).map((name) => {
+        const newStat: LeaderboardStat = {
+          username: user.username,
+          points: stats[name],
+        };
+
+        const curStats = leaderboardmap.get(name);
+
+        if (curStats.lowest > newStat.points) return;
+
+        curStats.values.push();
+      });
+    });
+
+    console.log(leaderboardmap);
+    return leaderboardmap;
   }
 }
