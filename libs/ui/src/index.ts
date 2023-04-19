29019d55e2039fc5f4988aa96ee41695e2b13bb8
Dan <22224922+DNYLA@users.noreply.github.com>
2023-04-19 10:16:29 +0100

    Added Skeleton Loading

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -6,6 +6,7 @@ export * from './lib/forms/FileUpload';
 export * from './lib/profile/header';
 export * from './lib/profile/stats';
 export * from './lib/profile/match-history';
+export * from './lib/profile/skeleton-table';
 export * from './lib/profile/comments-section';
 export * from './lib/profile/sidebar';
 
